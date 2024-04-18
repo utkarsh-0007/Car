0fa6974d4fb8a04b67e6573ff6559a75def9f562
Utkarsh <lovekickstartsyadav@gmail.com>
2024-04-18 20:48:26 +0530

    all

--- problem6.js
+++ problem6.js
@@ -2,4 +2,4 @@ function bmwAndAudi(inventory) {
     let bmwAudi= inventory.filter(car => car.car_make === 'BMW' || car.car_make === 'Audi');
     console.log(JSON.stringify(bmwAudi));  
 }
-module.exports=bmwAndAudi;
+module.exports= bmwAndAudi;
