3afb4e9da5f0ee6a2d8767d90ce2b75cd516fce7
Utkarsh <lovekickstartsyadav@gmail.com>
2024-04-18 20:54:50 +0530

    all

--- problem5.js
+++ problem5.js
@@ -3,4 +3,4 @@ const count = inventory.filter(car => car.car_year > year).length;
 const arr = inventory.filter(car => car.car_year > year);
 console.log(`Solution 5: Number of cars older than ${year} is ${count}`)
 }
-module.exports=count;
+module.exports= count;
